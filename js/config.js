e1253670d19c8ec6fd8134d11948545aeb7534ca
larissanaga-cmyk <larissa.naga@gmail.com>
2026-08-28 08:51:31 -0300

    config: vincular RSVP ao Google Apps Script

--- js/config.js
+++ js/config.js
@@ -3,10 +3,10 @@
   IMPORTANTE:
   - Nunca colocar Access Token do Mercado Pago neste arquivo.
   - O Access Token ficará apenas no backend/Cloudflare Worker.
-  - GOOGLE_SCRIPT_URL poderá receber a URL pública do Apps Script do RSVP.
+  - GOOGLE_SCRIPT_URL recebe a URL pública do Apps Script do RSVP.
 */
 window.WEDDING_CONFIG = {
-  GOOGLE_SCRIPT_URL: "",
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyFgRi30pMGKWrkqRyEkltDASbHIDSqDG1OLHtFHVZeZwfy_PwDBYuMfZNhm1jYvmR3/exec",
   PAYMENT_API_URL: "",
   MUSIC_SRC: "",
   MAP_URL: "https://share.google/CtQGtm6MDaeGIzhuj"
