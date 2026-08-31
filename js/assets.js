2aff512df7f530d45a1ee21f6c723c512b2e4486
larissanaga-cmyk <larissa.naga@gmail.com>
2026-08-31 20:20:35 -0300

    Refresh R$5 production test item

--- js/assets.js
+++ js/assets.js
@@ -56,11 +56,11 @@
   window.addEventListener("load", () => {
     const style = document.createElement("link");
     style.rel = "stylesheet";
-    style.href = "css/gifts-extended.css?v=8";
+    style.href = "css/gifts-extended.css?v=9";
     document.head.appendChild(style);
 
     const script = document.createElement("script");
-    script.src = "js/gifts-extended.js?v=8";
+    script.src = "js/gifts-extended.js?v=9";
     document.body.appendChild(script);
   });
 })();
