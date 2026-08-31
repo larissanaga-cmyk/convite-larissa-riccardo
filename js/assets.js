72eb6597f2a76de590edde2a8fe774fec72828cc
larissanaga-cmyk <larissa.naga@gmail.com>
2026-08-31 20:34:20 -0300

    Refresh gift list after final price adjustments

--- js/assets.js
+++ js/assets.js
@@ -56,11 +56,11 @@
   window.addEventListener("load", () => {
     const style = document.createElement("link");
     style.rel = "stylesheet";
-    style.href = "css/gifts-extended.css?v=9";
+    style.href = "css/gifts-extended.css?v=10";
     document.head.appendChild(style);
 
     const script = document.createElement("script");
-    script.src = "js/gifts-extended.js?v=9";
+    script.src = "js/gifts-extended.js?v=10";
     document.body.appendChild(script);
   });
 })();
