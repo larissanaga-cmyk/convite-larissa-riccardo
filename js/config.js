a96b157d1376f0cb85f44fa563088e18c81712a3
larissanaga-cmyk <larissa.naga@gmail.com>
2026-09-01 14:32:56 -0300

    Ativa música de fundo do convite

--- js/config.js
+++ js/config.js
@@ -9,6 +9,6 @@ window.WEDDING_CONFIG = {
   GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyFgRi30pMGKWrkqRyEkltDASbHIDSqDG1OLHtFHVZeZwfy_PwDBYuMfZNhm1jYvmR3/exec",
   PAYMENT_API_URL: "https://convite-mercadopago.larissa-naga.workers.dev/",
   PIX_KEY: "59395b0d-0a73-43ae-b554-a6fd81dc7c6d",
-  MUSIC_SRC: "",
+  MUSIC_SRC: "Mu%CC%81sica%20site.mp3",
   MAP_URL: "https://share.google/CtQGtm6MDaeGIzhuj"
 };
